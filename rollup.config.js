2be499fec1fa3727cdde6267a2a09196267d5fae
lykmapipo <lallyelias87@gmail.com>
2020-07-22 17:07:39 +0300

    chore(config): correct build external

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,7 @@ import pkg from './package.json';
 export default [
   {
     input: 'src/index.js',
-    external: [
-      'http',
-      'https',
-      ...keys(pkg.dependencies),
-      ...keys(pkg.peerDependencies),
-    ],
+    external: [...keys(pkg.dependencies), ...keys(pkg.peerDependencies)],
     output: [
       {
         file: pkg.main,
